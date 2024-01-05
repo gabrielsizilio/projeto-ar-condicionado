d9b58febeb44593b1b134ec00a06840b14c02410
Gabriel Sizilio <gabrielsizilio15@gmail.com>
2024-01-05 16:27:43 -0300

    add páginas de login

--- src/routes/login.js
+++ src/routes/login.js
@@ -1,11 +1,16 @@
 const express = require('express')
+const { joint, join } = require('node:path')
 const router = express.Router()
 const authController = require('../controllers/AuthController')
 
-router.post('/', authController.login)
+// router.post('/', authController.login)
 // router.get('/', (req, res) => {
 //     res.json({ Pagina: 'login' })
 // })
 
+router.get('/', (req, res) => {
+    console.log();
+    res.sendFile(join(__dirname, '../..', 'public', 'html', 'login.html'))
+})
 
 module.exports = router
