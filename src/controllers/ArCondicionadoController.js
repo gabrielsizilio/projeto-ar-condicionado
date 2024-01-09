39011bbe0fe4b3e39dfb0ec2f76336127f1fa5f1
Gabriel Sizilio <gabrielsizilio15@gmail.com>
2024-01-09 14:21:53 -0300

    edit, update e remove prontos

--- src/controllers/ArCondicionadoController.js
+++ src/controllers/ArCondicionadoController.js
@@ -22,12 +22,12 @@ async function create(req, res) {
     }
 }
 
-function edit(req, res) {
-
-}
-
 async function store(req, res) {
     const { nome, modelo, marca_id } = req.body
+
+    if (!nome || !modelo || !marca_id) {
+        return res.status(400).json({ msgErr: 'Campos obrigatórios não preenchidos.' })
+    }
 
     try {
         const marca = await Marca.findByPk(marca_id)
@@ -44,19 +44,89 @@ async function store(req, res) {
     }
 }
 
-function update(req, res) {
+async function edit(req, res) {
+    const ar_id = req.params
 
+    try {
+        arCondicionado = await ArCondicionado.findOne({
+            where: { id: ar_id.id },
+            include: [{ model: Marca, as: 'marca' }]
+        })
+
+        if (!arCondicionado) {
+            return res.status(404).json({ msgErr: 'Ar-condicionado não cadastrado no sistema' })
+        }
+
+        res.json(arCondicionado)
+    } catch (error) {
+        console.log(error)
+    }
 }
 
-function remove(req, res) {
 
+async function update(req, res) {
+    const ar_id = req.params
+    const { nome, modelo, marca_id } = req.body
+
+    if (!nome || !modelo || !marca_id) {
+        return res.status(400).json({ msgErr: 'Campos obrigatórios não preenchidos.' })
+    }
+
+    if (!ar_id.id) {
+        return res.status(400).json({ msgErr: 'É necessário informar qual o ar-condicionado a ser editado.' })
+    }
+
+    try {
+        const arCondicionado = await ArCondicionado.findOne({
+            where: { id: ar_id.id },
+            include: [{ model: Marca, as: 'marca' }]
+        })
+
+        if (!arCondicionado) {
+            return res.status(404).json({ msgErr: 'Ar-condicionado não encontrado.' })
+        }
+
+        await arCondicionado.update({
+            nome,
+            modelo,
+            marca_id
+        })
+
+        return res.status(200).json({ arCondicionado })
+
+    } catch (error) {
+        return res.status(500).json({ msgErr: "Ocorreu um erro! ", error });
+    }
+}
+
+async function remove(req, res) {
+    const ar_id = req.params
+
+    if (!ar_id.id) {
+        return res.status(400).json({ msgErr: 'É necessário informar qual o ar-condicionado a ser removido.' })
+    }
+
+    try {
+        const arCondicionado = await ArCondicionado.findByPk(ar_id.id)
+
+        if (!arCondicionado) {
+            return res.status(404).json({ msgErr: 'Ar-condicionado não encontrado.' })
+        }
+
+        await arCondicionado.destroy()
+
+        res.json({msg: "Ar-condicionado excluído do sistema"})
+        
+    } catch (error) {
+        return res.status(500).json({ msgErr: "Ocorreu um erro! ", error });
+    }
 }
 
 module.exports = {
     index,
     create,
-    edit,
     store,
+    edit,
     update,
     remove
 }
--- src/routes/ar-condicionado.js
+++ src/routes/ar-condicionado.js
@@ -6,6 +6,12 @@ router.get('/', arCondiciandoController.index)
 
 router.get('/create', arCondiciandoController.create)
 
-router.post('/save', arCondiciandoController.store)
+router.post('/store', arCondiciandoController.store)
+
+router.get('/edit/:id', arCondiciandoController.edit)
+
+router.post('/update/:id', arCondiciandoController.update)
+
+router.post('/remove/:id', arCondiciandoController.remove)
 
 module.exports = router
