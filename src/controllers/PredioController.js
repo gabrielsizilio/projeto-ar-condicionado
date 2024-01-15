d10347b57dcff9cd76cdd42507e77c0dd0200151
Gabriel Sizilio <gabrielsizilio15@gmail.com>
2024-01-15 12:11:08 -0300

    wip: cria controller de entre salas e predios

---
+++ src/controllers/PredioController.js
@@ -0,0 +1,107 @@
+const Predio = require('../models/Predio')
+
+
+async function index(req, res) {
+
+}
+
+async function create(req, res) {
+
+}
+
+async function store(req, res) {
+    const { nome } = req.body
+
+    if (!nome) {
+        return res.status(400).json({ msgErr: 'Campos obrigatórios não preenchidos.' })
+    }
+
+    try {
+        await Predio.create({
+            nome
+        })
+
+        res.redirect('/ar-condicionado')
+    } catch (error) {
+        res.status(500).json({ msgErr: 'Ocorreu um erro: ', error })
+    }
+}
+
+async function edit(req, res) {
+    const predio_id = req.params
+
+    try {
+        const predio = await predio.findByPk(predio_id.id)
+
+        if (!predio) {
+            return res.status(404).json({ msgErr: 'Prédio não cadastrado no sistema' })
+        }
+
+        return res.json(predio)
+    } catch (error) {
+        console.log(error)
+    }
+}
+
+
+async function update(req, res) {
+    const predio_id = req.params
+    const { nome } = req.body
+
+    if (!nome) {
+        return res.status(400).json({ msgErr: 'Campos obrigatórios não preenchidos.' })
+    }
+
+    if (!predio_id.id) {
+        return res.status(400).json({ msgErr: 'É necessário informar qual predio será editado.' })
+    }
+
+    try {
+        const predio = await predio.findByPk(predio_id.id)
+
+        if (!predio) {
+            return res.status(404).json({ msgErr: 'Prédio não encontrado.' })
+        }
+
+        await predio.update({
+            nome
+        })
+
+        return res.status(200).json({ predio })
+
+    } catch (error) {
+        return res.status(500).json({ msgErr: "Ocorreu um erro! ", error });
+    }
+}
+
+async function remove(req, res) {
+    const predio_id = req.params
+
+    if (!predio_id.id) {
+        return res.status(400).json({ msgErr: 'É necessário informar qual predio será removida.' })
+    }
+
+    try {
+        const predio = await predio.findByPk(predio_id.id)
+
+        if (!predio) {
+            return res.status(404).json({ msgErr: 'Prédio não encontrada.' })
+        }
+
+        await predio.destroy()
+
+        res.json({ msg: "Prédio excluída do sistema" })
+
+    } catch (error) {
+        return res.status(500).json({ msgErr: "Ocorreu um erro! ", error });
+    }
+}
+
+module.exports = {
+    index,
+    create,
+    store,
+    edit,
+    update,
+    remove
+}
--- src/controllers/SalaController.js
+++ src/controllers/SalaController.js
@@ -1,44 +1,114 @@
+const Predio = require('../models/Predio')
+const Sala = require('../models/Sala')
 
-class SalaController {
-    async index (req, res) {
-        try{
-            res.render('sala/index');
-        }catch (error) {
-            console.log(error);
-        }
-    }
-    
-    async create (req, res) {
-        try {   
-            res.render('sala/create');
-        } catch (error) {
-            console.log(error);
-        }   
-    }
 
-    async edit(req, res) {
-        try {
-            res.render('sala/edit');
-        } catch (error) {
-            console.log(error);
-        }
+async function index(req, res) {
+
+}
+
+async function create(req, res) {
+
+}
+
+async function store(req, res) {
+    const { nome, predio_id } = req.body
+
+    if (!nome || !predio_id) {
+        return res.status(400).json({ msgErr: 'Campos obrigatórios não preenchidos.' })
     }
 
-    async update(req, res) {
-        try {
+    try {
+        await Sala.create({
+            nome,
+            predio_id
+        })
 
-        } catch (error) {
-            console.log(error)
-        }
-    }
-
-    async delete (req, res) {
-        try{
-
-        } catch (error) {
-            console.log(error);
-        }
+        res.redirect('/ar-condicionado')
+    } catch (error) {
+        res.status(500).json({ msgErr: 'Ocorreu um erro: ', error })
     }
 }
 
-module.exports = new SalaController();
+async function edit(req, res) {
+    const sala_id = req.params
+
+    try {
+        const sala =  await Sala.findOne({
+            where: { id: sala_id.id },
+            include: [{ model: Predio, as: 'predio' }]
+        })
+
+        const predios = await Predio.findAll();
+
+        if (!sala) {
+            return res.status(404).json({ msgErr: 'Sala não cadastrada no sistema' })
+        }
+
+        return res.json(sala, predios)
+    } catch (error) {
+        console.log(error)
+    }
+}
+
+async function update(req, res) {
+    const sala_id = req.params
+    const { nome, predio_id } = req.body
+
+    if (!nome || !predio_id) {
+        return res.status(400).json({ msgErr: 'Campos obrigatórios não preenchidos.' })
+    }
+
+    if (!sala_id.id) {
+        return res.status(400).json({ msgErr: 'É necessário informar qual sala será editada.' })
+    }
+
+    try {
+        const sala = await Sala.findByPk(marca_id.id)
+
+        if (!sala) {
+            return res.status(404).json({ msgErr: 'Sala não encontrado.' })
+        }
+
+        await sala.update({
+            nome,
+            predio_id
+        })
+
+        return res.status(200).json({ sala })
+
+    } catch (error) {
+        return res.status(500).json({ msgErr: "Ocorreu um erro! ", error });
+    }
+}
+
+async function remove(req, res) {
+    const sala_id = req.params
+
+    if (!sala_id.id) {
+        return res.status(400).json({ msgErr: 'É necessário informar qual sala será removida.' })
+    }
+
+    try {
+        const sala = await Sala.findByPk(sala_id.id)
+
+        if (!sala) {
+            return res.status(404).json({ msgErr: 'Sala não encontrada.' })
+        }
+
+        await sala.destroy()
+
+        res.json({ msg: "Sala excluída do sistema" })
+
+    } catch (error) {
+        return res.status(500).json({ msgErr: "Ocorreu um erro! ", error });
+    }
+}
+
+module.exports = {
+    index,
+    create,
+    store,
+    edit,
+    update,
+    remove
+}
