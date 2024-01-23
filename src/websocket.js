aaefbb772494e440137e015f6ecf003e5fbdece1
yodemisj <114534608+yodemisj@users.noreply.github.com>
2024-01-23 17:46:58 -0300

    Iniciacao do código dos websockets

--- src/websocket.js
+++ src/websocket.js
@@ -1,16 +1,42 @@
 const { io } = require('./http')
 
+const controladorSockets = {}
+
 io.on('connection', (socket) => {
-    console.log('a user connected');
-    console.log(socket.id);
+    console.log(`Um usuário se conectou { id: ${socket.id} }`)
+    
+    socket.on('registrarControlador', (id_controlador) => {
+        console.log(`Controlador registrado: ${id_controlador}`)
+        controladorSockets[id_controlador] = socket.id
+        console.log(controladorSockets);
+    })
+    
+    socket.on('enviarComandoAr', (comando) => {
+        console.log(comando);
 
-    socket.on('button', (temperatura) => {
-        
-        io.emit('button', temperatura);
-        console.log('the user click in the button');
-    });
+        const codigo_ir = [];
+        // Buscar Código IR para cada Ar condicionado
 
+        // codigo_ir.push()
+        // codigo_ir.push()
+
+        if(!controladorSockets[comando.id_controlador]) {
+            console.log("Deu Ruim controlador não encontrado");
+            // Precisa tratar o erro corretamente
+            return;
+        }
+
+        io.to(controladorSockets[comando.id_controlador]).emit('EnviaIR',codigo_ir);
+    })
+    
     socket.on('disconnect', () => {
-        console.log('user disconnected');
-    });
+        console.log(`Usuário desconectado { id: ${socket.id} }`)
+
+        Object.keys(controladorSockets).forEach(id_controlador => {
+            if(controladorSockets[id_controlador] == socket.id) {
+                delete controladorSockets[id_controlador]
+            }
+        });
+        console.log(controladorSockets);
+    })
 });
