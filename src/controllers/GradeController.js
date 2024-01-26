73e514674591d0cafd933575368ffeda4d1726ef
Gabriel Sizilio <gabrielsizilio15@gmail.com>
2024-01-26 11:19:01 -0300

    refact: refatora query de busca de alocao_horarios

--- src/controllers/GradeController.js
+++ src/controllers/GradeController.js
@@ -5,15 +5,15 @@ const AlocacaoHorario = require('../models/AlocacaoHorario')
 
 async function index(req, res) {
 
-    const usuarios = await User.findAll({include: [{association: 'credencial'}]})
+    const user = await User.findAll({include: [{association: 'credencial'}]})
     const predios = await Predio.findAll({
         include: { association: 'salas' }
     })
     const semana = await Semana.findAll()
-    const alocaoHorarios = await AlocacaoHorario.findAll()
-
+    const alocaoHorarios = await AlocacaoHorario.sequelize.query("SELECT * FROM alocacao_horarios")
+    
     try {
-        res.status(200).render('grade/index', {usuarios, predios, semana, alocaoHorarios})
+        res.status(200).render('grade/index', {user , predios, semana, alocaoHorarios})
     } catch (error) {
         console.error('Ocorreu um erro: ', error);
     }
