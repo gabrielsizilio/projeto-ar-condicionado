e96fe17666b668eb7436d2981b3a40ecedd809cf
yodemisj <114534608+yodemisj@users.noreply.github.com>
2024-01-31 01:54:18 -0300

    Atualização do código do arduino, porem ainda com memoria insuficiente

--- scketchs/arduino/Arduino-ligaledWifi/Arduino-ligaledWifi.ino
+++ scketchs/arduino/Arduino-ligaledWifi/Arduino-ligaledWifi.ino
@@ -1,77 +1,95 @@
-#define pinLed 8
-
 #include <IRremote.h>
-#include <ArduinoJson.h>
+#define MAX_IR_SIZE 200
+#define MAX_MESSAGE_SIZE 1050
 
 IRsend irsend;
-int khz = 38;
 
-const int maxStringSize = 1000;
-char receivedString[maxStringSize];
-
-int ligado = 0;
-void alternaLed() {
-  if (!ligado) {
-    ligado = 1;
-    digitalWrite(pinLed, HIGH);
-  } else {
-    ligado = 0;
-    digitalWrite(pinLed, LOW);
-  }
-}
-
-void parseList(String listString, int* intArray, int arraySize) {
-  int index = 0;
-  int start = 1;  // Ignora o primeiro caractere '['
-
-  // Percorre a string, separando os elementos por vírgula e convertendo para inteiros
-  for (int i = 1; i < listString.length(); i++) {
-    if (listString[i] == ',' || listString[i] == ']') {
-      listString[i] = '\0';                          // Substitui ',' ou ']' por '\0' para separar os elementos
-      intArray[index++] = atoi(&listString[start]);  // Converte e adiciona o elemento ao vetor de inteiros
-      start = i + 1;                                 // Atualiza o início para o próximo elemento
-    }
-    if (index >= arraySize) break;  // Sai do loop se o vetor estiver cheio
-  }
-}
+unsigned int IRSignal[MAX_IR_SIZE];
+char receivedMessage[MAX_MESSAGE_SIZE];
+int messageIndex = 0;
+int irIndex = 0;
 
 void setup() {
   Serial.begin(9600);
-  pinMode(pinLed, OUTPUT);
 }
 
 void loop() {
-  String mensagem = Serial.readString();
-  String eu = "led";
+    int khz = 38;
+    // unsigned int IrLength = 0;
+    // if (Serial.available() > 0) {
+    //     // Lê a mensagem recebida da porta serial
+    //     String receivedMessage = Serial.readStringUntil('\n');
+    //     // Serial.println(receivedMessage);
 
-  mensagem.trim();
+    //     if (receivedMessage.startsWith("[\"EnviaIR\"")) {
+    //         int tamanho = receivedMessage.length();
+    //         // Serial.println("Tá entrando no IF!!");
+    //         // receivedMessage = receivedMessage.substring(12);
+    //         // receivedMessage.remove(tamanho - 1);
+    //         Serial.println(receivedMessage);
+    //     }
+    //     //     // Serial.println(receivedMessage);
+    //     //     int tamanho = receivedMessage.length();
+    //     //     Serial.println(tamanho);
 
-  if (mensagem.equals(eu)) {
-    // irsend.sendRaw(irSignal, sizeof(irSignal) / sizeof(irSignal[0]), khz);
-    alternaLed();
-  }
+    //     //     char teste[10];
+    //     //     int index = 0;
+    //         // for(int i=0; i<tamanho; i++) {
+    //             // char  c = receivedMessage[i];
+                
+    //             // if (c == ',' || c == ']') {
+    //                 // Serial.print("NUMERO EM STRING: ");
+    //                 // Serial.println(teste);
+    //                 // IRSignal[IrLength++] = atoi(teste);
+    //                 // IrLength++;
+    //                 // memset(teste, 0, sizeof(teste));
+    //                 // index = 0;
+    //                 // Serial.print("IrLength: ");
+    //                 // Serial.println(IrLength);
+    //                 // return;
+    //             // } else {
+    //             //     teste[index++] = c;
+    //             // }
+    //         // }
+    //         // Serial.println(receivedMessage);
+    //     // }   
+    // }
 
 
+    if (Serial.available() > 0) {
+    char c = Serial.read();  
 
+    if (c == '\n' || messageIndex >= MAX_MESSAGE_SIZE - 1) {
+        receivedMessage[messageIndex] = '\0';  
 
-  String received = "[\"EnviaIR\",[\"5960, 7044, 608, 1564, 576, 1564, 580, 1564, 580, 1564, 576, 1564, 576, 1568, 576, 1564, 580, 1564, 576, 508, 576, 512, 572, 508, 576, 508, 576, 508, 576, 508, 576, 508, 576, 508, 576, 1564, 576, 1568, 576, 1568, 572, 1568, 576, 1568, 572, 1568, 576, 1568, 572, 1568, 572, 512, 572, 512, 548, 536, 548, 536, 548, 536, 548, 536, 548, 536, 548, 536, 544, 1600, 544, 1572, 568, 1576, 568, 1572, 544, 1600, 540, 1600, 544, 1596, 544, 1600, 568, 544, 540, 544, 540, 544, 540, 544, 540, 544, 540, 544, 540, 544, 540, 544, 540, 1576, 568, 1572, 568, 520, 564, 1576, 568, 1572, 568, 1576, 564, 1576, 568, 1576, 564, 520, 564, 520, 564, 1576, 568, 520, 560, 524, 564, 520, 560, 524, 564, 520, 560, 524, 560, 1580, 564, 1580, 560, 524, 560, 1580, 564, 520, 560, 1584, 560, 1584, 556, 1584, 536, 548, 536, 548, 536, 1608, 556, 528, 556, 1588, 532, 552, 528, 556, 528, 556, 528, 1612, 532, 576, 508, 1632, 508, 576, 508, 1636, 508, 576, 508, 576, 508, 1632, 508, 576, 508, 1636, 508, 576, 508, 1636, 504, 580, 504, 1636, 508, 1636, 504, 7160, 504\"]]";
+        if (strncmp(receivedMessage, "[\"EnviaIR\"", 10) == 0) {
+            Serial.println("Mensagem \"EnviaIR\" recebida.");
 
-  StaticJsonDocument<maxStringSize> doc;
-  deserializeJson(doc, received);
+            char buffer[10];
+             unsigned int bufferIndex = 0;
 
-  String action = doc[0];
-  Serial.println("Ação: " + action);
+            for(int i=12; i<messageIndex-1; i++) {
+                if(receivedMessage[i] == ',' || receivedMessage[i] == ']') {
+                    IRSignal[irIndex++] = atoi(buffer);
+                    // Serial.print("String dentro do vetor: ");
+                    // Serial.println(IRSignal[irIndex-1]);
+                    memset(buffer, 0, sizeof(buffer));
+                    bufferIndex = 0;
+                } else {
+                    buffer[bufferIndex++] = receivedMessage[i];
+                }
+            }
+            // Serial.println(IRSignal[irIndex-1]);
+            irsend.sendRaw(IRSignal, sizeof(IRSignal) / sizeof(IRSignal[0]), khz);
 
-  String listString = doc[1];
-  int intArray[200];  
-  parseList(listString, intArray, 200);
-
-  Serial.println("Elementos do vetor:");
-  for (int i = 0; i < 200; i++) {
-        if (intArray[i] != 0) {
-            Serial.println(intArray[i]);
         } else {
-            break; 
+            Serial.println("Mensagem não reconhecida.");
         }
+
+        messageIndex = 0;  
+    } else {
+      receivedMessage[messageIndex++] = c;
+    }
   }
 }
+
