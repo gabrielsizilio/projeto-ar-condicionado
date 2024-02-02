824dcfd07941a06e8391f2c25021464d778c8702
yodemisj <114534608+yodemisj@users.noreply.github.com>
2024-02-02 11:51:41 -0300

    remove models do websocket

--- src/websocket.js
+++ src/websocket.js
@@ -3,18 +3,12 @@ const { io } = require('./http')
 const macAddressMapping = {}
 const SocketController = require('./controllers/SocketController');
 
-const ArCondicionado = require('./models/ArCondicionado')
-const Modelo = require('./models/Modelo')
-const Temperatura = require('./models/Temperatura')
-
-
-
 io.on('connection', (socket) => {
     console.log(`Um usuário se conectou { id: ${socket.id} }`)
 
-    socket.on('setup', (esp)=> { SocketController.setup(esp, socket, macAddressMapping) });
+    socket.on('setup', (esp) => { SocketController.setup(esp, socket, macAddressMapping) });
 
-    socket.on('enviarComandoAr', async (comando)=> { SocketController.enviaComando(comando, macAddressMapping , io) });
+    socket.on('enviarComandoAr', async (comando) => { SocketController.enviaComando(comando, macAddressMapping , io) });
 
     socket.on('disconnect', () => { SocketController.disconnect(socket, macAddressMapping) });
 
