7136c033154a4a44a44cecdab151ea003cfdb4ff
yodemisj <114534608+yodemisj@users.noreply.github.com>
2024-02-22 05:47:34 -0300

    socketController correção da função enviaComando

--- src/controllers/SocketController.js
+++ src/controllers/SocketController.js
@@ -12,38 +12,37 @@ class SocketController {
 
     async enviaComando(comando, macAddressMapping, io) {
 
-        // var aparelho = await ArCondicionado.findOne({
-        //     include: [{
-        //         model: Modelo, as: 'modelo',
-        //         include: [{
-        //             model: Temperatura, as: 'temperatura'
-        //         }]
-        //     }]
-        // });
+        var aparelho = await ArCondicionado.findByPk(comando.id_arcondicionado, {
+                include: [{
+                    model: Modelo, as: 'modelo',
+                    include: [{
+                        model: Temperatura, as: 'temperatura'
+                    }]
+                }]
+        });
 
-        // var temp16 = aparelho.modelo.temperatura.temp16;
+        let codigo_ir;
+        let temperatura;
 
-        const codigo_ir = [];
-        // ['5960, 7044, 608, 1564, 576, 1564, 580, 1564, 580, 1564, 576, 1564, 576, 1568, 576, 1564, 580, 1564, 576, 508, 576, 512, 572, 508, 576, 508, 576, 508, 576, 508, 576, 508, 576, 508, 576, 1564, 576, 1568, 576, 1568, 572, 1568, 576, 1568, 572, 1568, 576, 1568, 572, 1568, 572, 512, 572, 512, 548, 536, 548, 536, 548, 536, 548, 536, 548, 536, 548, 536, 544, 1600, 544, 1572, 568, 1576, 568, 1572, 544, 1600, 540, 1600, 544, 1596, 544, 1600, 568, 544, 540, 544, 540, 544, 540, 544, 540, 544, 540, 544, 540, 544, 540, 544, 540, 1576, 568, 1572, 568, 520, 564, 1576, 568, 1572, 568, 1576, 564, 1576, 568, 1576, 564, 520, 564, 520, 564, 1576, 568, 520, 560, 524, 564, 520, 560, 524, 564, 520, 560, 524, 560, 1580, 564, 1580, 560, 524, 560, 1580, 564, 520, 560, 1584, 560, 1584, 556, 1584, 536, 548, 536, 548, 536, 1608, 556, 528, 556, 1588, 532, 552, 528, 556, 528, 556, 528, 1612, 532, 576, 508, 1632, 508, 576, 508, 1636, 508, 576, 508, 576, 508, 1632, 508, 576, 508, 1636, 508, 576, 508, 1636, 504, 580, 504, 1636, 508, 1636, 504, 7160, 504'];
-        // Buscar Código IR para cada Ar condicionado
+        if(comando.temperatura.trim() != "off"){
+            temperatura = "temp" + comando.temperatura.replace('°C', '');
+        } else {
+            temperatura = "off";
+        }
 
-        const stringNumeros = '5960, 7044, 608, 1564, 576, 1564, 580, 1564, 580, 1564, 576, 1564, 576, 1568, 576, 1564, 580, 1564, 576, 508, 576, 512, 572, 508, 576, 508, 576, 508, 576, 508, 576, 508, 576, 508, 576, 1564, 576, 1568, 576, 1568, 572, 1568, 576, 1568, 572, 1568, 576, 1568, 572, 1568, 572, 512, 572, 512, 548, 536, 548, 536, 548, 536, 548, 536, 548, 536, 548, 536, 544, 1600, 544, 1572, 568, 1576, 568, 1572, 544, 1600, 540, 1600, 544, 1596, 544, 1600, 568, 544, 540, 544, 540, 544, 540, 544, 540, 544, 540, 544, 540, 544, 540, 544, 540, 1576, 568, 1572, 568, 520, 564, 1576, 568, 1572, 568, 1576, 564, 1576, 568, 1576, 564, 520, 564, 520, 564, 1576, 568, 520, 560, 524, 564, 520, 560, 524, 564, 520, 560, 524, 560, 1580, 564, 1580, 560, 524, 560, 1580, 564, 520, 560, 1584, 560, 1584, 556, 1584, 536, 548, 536, 548, 536, 1608, 556, 528, 556, 1588, 532, 552, 528, 556, 528, 556, 528, 1612, 532, 576, 508, 1632, 508, 576, 508, 1636, 508, 576, 508, 576, 508, 1632, 508, 576, 508, 1636, 508, 576, 508, 1636, 504, 580, 504, 1636, 508, 1636, 504, 7160, 504';
+        codigo_ir  = aparelho.modelo.temperatura[temperatura];
 
-        let numeros = stringNumeros.split(',').map(numero => parseInt(numero.trim()));
+        codigo_ir = codigo_ir.split(',').map(numero => parseInt(numero.trim()));
 
-        console.log(numeros.length);
-        console.log(numeros);
-
-        // codigo_ir.push(temp16)
-        // console.log(codigo_ir[0]);
-        codigo_ir.push(numeros)
+        console.log(codigo_ir.length);
+        console.log(codigo_ir);
 
         if (!macAddressMapping[comando.id_controlador]) {
             console.log("Deu Ruim controlador não encontrado");
             // Precisa tratar o erro corretamente
             return;
         }
-        io.to(macAddressMapping[comando.id_controlador]).emit('EnviaIR', numeros)
+        io.to(macAddressMapping[comando.id_controlador]).emit('EnviaIR', codigo_ir)
     }
 
     disconnect(socket, macAddressMapping) {
