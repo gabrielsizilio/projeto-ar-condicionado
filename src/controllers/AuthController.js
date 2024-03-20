71a9da68db1a760baab5b2033900996d8b92d2e2
Gabriel Sizilio <gabrielsizilio15@gmail.com>
2024-03-20 20:43:30 -0300

    feat: add log when login

--- src/controllers/AuthController.js
+++ src/controllers/AuthController.js
@@ -2,6 +2,8 @@ const bcrypt = require('bcryptjs')
 const jwt = require('jsonwebtoken')
 const Credencial = require('../models/Credencial')
 const { createToken } = require('../config/auth')
+const Log = require('../models/Log')
+const Usuario = require('../models/Usuario')
 
 function index(req, res) {
     return res.render('login')
@@ -34,8 +36,18 @@ async function login(req, res) {
             if (token == 500) {
                 res.status(500).json({ msgErr: "Ocorreu um erro ao criar o token!" })
             } else {
+                
+                // TODO: alterar para middleware de logs
+                const usuario = await Usuario.findOne({where: {credencial_id: credencial.id}})
+
+                await Log.create({
+                    descricao: "Efetuou login",
+                    usuario_id: credencial.id
+                })
+
                 // res.header('Authorization', `Bearer ${token}`);
-                res.cookie('jwt', token, {/*secure: true,*/ httpOnly: true, maxAge: process.env.AUTH_EXPIRE_TOKEN * 1000 })
+                // TODO: colcoar 'secure: true' qnd der deploy
+                res.cookie('jwt', token, {/* secure: true,*/ httpOnly: true, maxAge: process.env.AUTH_EXPIRE_TOKEN * 1000 })
                 res.status(200).json({ msg: "Logado com sucesso!", success: true })
 
                 // return res.redirect('/')
