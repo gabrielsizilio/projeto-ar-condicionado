52deb8e322894da4c157e71a9a502299d0a8ea01
Gabriel Sizilio <gabrielsizilio15@gmail.com>
2024-04-17 19:51:30 -0300

    feat: add authorization

--- src/middleware/accessMiddleware.js
+++ src/middleware/accessMiddleware.js
@@ -1,13 +1,23 @@
 const { checkToken } = require("../config/auth");
+const Role = require("../models/Role");
 const Usuario = require("../models/Usuario");
 
 function access(allowedRoles) {
     return async (req, res, next) => {
-        const token = req.cookies.jwt
-        const payload = checkToken(token)
 
-        const hasAccess = allowedRoles.includes(payload.role)
-        
+        let hasAccess = false;
+
+        if(req.cookies.jwt) {
+            const token = req.cookies.jwt;
+            const payload = checkToken(token);
+            
+            hasAccess = allowedRoles.includes(payload.role);
+        } else if(req.user) {
+            const role = await Role.findByPk(req.user.role_id);
+
+            hasAccess = allowedRoles.includes(role.nome);
+        }
+
         if(hasAccess) {
             return next()
         } else {
