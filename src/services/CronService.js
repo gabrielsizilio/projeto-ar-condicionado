894d2ffc5ca268bdc820257b5c4bc42a0da847d1
Gabriel Sizilio <gabrielsizilio15@gmail.com>
2025-01-16 14:07:53 -0300

    feat: Add converter date to cron expression

--- src/services/CronService.js
+++ src/services/CronService.js
@@ -2,18 +2,37 @@ var cron = require("node-cron");
 const { getAllTasks, getAllTasksPending } = require("./TaskService");
 getTasks();
 
+function dateToCron(dateTime) {
+    const date = new Date(dateTime);
+    const seconds = date.getUTCSeconds();
+    const minutes = date.getUTCMinutes();
+    const hours = date.getUTCHours();
+    const dayOfMonth = date.getUTCDate();
+    const month = date.getUTCMonth() + 1;
+
+    return `${seconds} ${minutes} ${hours} ${dayOfMonth} ${month} *`;
+}
+
 let tasks = [];
 async function getTasks() {
     try {
         tasks = await getAllTasksPending();
-        console.log(">> TASK FINDEED:", JSON.stringify(tasks, null, 2));
+
+        tasks.forEach((task, index) => {
+            const cronExpression = dateToCron(task.dateTime);
+
+            cron.schedule(cronExpression, () => {
+                const now = new Date();
+                console.log(`[${now}] >> RUNING TASK_ID: ${task.id}`);
+            });
+        });
+
+        console.log(">> TASK FINDEED:", tasks.length);
     } catch (error) {
         console.error(">> Erro ao buscar todas as tasks:: " + error);
-        
+
     }
 }
 
-cron.schedule("* * * * * *", () => {
-    console.log("RUNING" + JSON.stringify(tasks, null, 2));
-});
+
 
