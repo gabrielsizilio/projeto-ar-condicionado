96e5ae6b06e1b09db8e9517340e5c775ade0fd65
Gabriel Sizilio <gabrielsizilio15@gmail.com>
2025-01-17 13:12:04 -0300

    fix: fixes concurrency time on tasks

--- src/services/CronService.js
+++ src/services/CronService.js
@@ -12,22 +12,51 @@ function dateToCron(task, gap = 0) {
     let dayOfMonth = date.getUTCDate();
     let month = date.getUTCMonth() + 1;
 
-    if(task.type == "recurring") {
+    if (task.type == "recurring") {
         dayOfMonth = "*"
         month = "*"
     }
 
     const cronExpression = `${seconds} ${minutes} ${hours} ${dayOfMonth} ${month} *`
-    console.log(cronExpression);
+    // console.log(cronExpression);
 
     return cronExpression;
 }
 
+function verifyConcurrence(tasksPending) {
+    let taskMap = new Map();
+
+    tasksPending.forEach((task, taskIndex) => {
+        task.aresCondicionados.forEach((arCondicionado, index) => {
+            const newDateTime = new Date(task.dateTime);
+            newDateTime.setSeconds(newDateTime.getUTCSeconds() + index);
+
+            while (taskMap.has(newDateTime.toISOString())) {
+                newDateTime.setSeconds(newDateTime.getUTCSeconds() + 1);
+            }
+
+            if (!taskMap.has(newDateTime.toISOString())) {
+                taskMap.set(newDateTime.toISOString(), []);
+            }
+
+            taskMap.get(newDateTime.toISOString()).push({ arCondicionado, dateTime: newDateTime, taskIndex });
+        });
+    });
+
+    taskMap.forEach((tasks, key) => {
+        console.log(`Horário: ${key}`);
+        tasks.forEach((taskData, index) => {
+            console.log(`  Task ${taskData.taskIndex + 1}: ${taskData.arCondicionado.nome} às ${taskData.dateTime.toISOString()} \n`);
+        });
+    });
+}
+
+
 let tasks = [];
 async function verifyTasks() {
     try {
-        const dateTime = new Date('2025-01-17T11:23:30.000Z');
-        const dateTime2 = new Date('2025-01-17T11:23:31.000Z');
+        const dateTime = new Date('2025-01-17T12:02:00.000Z');
+        const dateTime2 = new Date('2025-01-17T12:02:01.000Z');
         const temperatura = 1;
         const aresCondicionadosId = [1, 2, 3, 4];
         const aresCondicionados2Id = [2, 3];
@@ -35,26 +64,34 @@ async function verifyTasks() {
         const type2 = "recurring"
 
         // await createTask(dateTime, temperatura, aresCondicionadosId, type2);
-        // await createTask(dateTime, temperatura, aresCondicionados2Id, type);
+        // await createTask(dateTime2, temperatura, aresCondicionados2Id, type);
 
         tasks = await getAllTasksPending();
+        console.log(">> TASK FINDEED:", tasks.length);
+        const taskMap = verifyConcurrence(tasks);
 
         let gap = 0;
+        let lastScheduledTime = {};
         tasks.forEach((task, index) => {
-            // console.log(task.aresCondicionados);
-
+            gap = 0;
             task.aresCondicionados.forEach((arCondicionado, index) => {
+                const taskKey = task.dateTime.toISOString();
+
+                if (lastScheduledTime[taskKey]) {
+                    gap += 1;
+                } else {
+                    lastScheduledTime[taskKey] = true;
+                }
+
                 const cronExpression = dateToCron(task, gap);
-                gap += 1;
 
                 cron.schedule(cronExpression, async () => {
                     await runTask(task, arCondicionado);
                 });
             })
         });
-        gap = 0;
 
-        console.log(">> TASK FINDEED:", tasks.length);
+
     } catch (error) {
         console.error(">> Erro ao verificar todas as tasks:: " + error);
 
